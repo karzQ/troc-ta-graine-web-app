5cbdd3f0a427a88dc603dd147b59cf6a37fdef08
karzQ <karczinski.quentin@gmail.com>
2019-03-20 01:08:28 +0100

    correction d'un bug dû à un changement de propriété dans la classe user et à une propriété mal nommée dans shareYourGarden.

--- client/app/models/mocks/users-mock.ts
+++ client/app/models/mocks/users-mock.ts
@@ -1,6 +1,8 @@
 import { User } from '../class/user';
 
 export const users: User[] = [
-    { id: 0, name: 'Vincent', lastname: 'TEST', accountCreationDate: '2019-03-12', birthDate: '1996-09-03' },
-    { id: 1, name: 'Quentin', lastname: 'TEST', accountCreationDate: '2017-11-25', birthDate: '1997-07-23' },
+    { id: 0, firstName: 'Vincent', lastName: 'TEST', accountCreationDate: '2019-03-12',
+    birthDate: '1996-09-03', username: 'quent1', password: 'test' },
+    { id: 1, firstName: 'Quentin', lastName: 'TEST', accountCreationDate: '2017-11-25',
+    birthDate: '1997-07-23', username: 'v1cent', password: 'test' },
 ];
